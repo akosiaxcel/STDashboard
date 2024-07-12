25e282ca93119e1e47281958b956a7ea2c96ccc8
akosiaxcel <117887168+akosiaxcel@users.noreply.github.com>
2024-07-12 05:28:10 +0000

    fetching successful

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -8,10 +8,11 @@ function Dashboard() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [searchId, setSearchId] = useState("");
+  const [searchIds, setSearchIds] = useState([]);
   const [searchError, setSearchError] = useState(null);
-  const [searchResult, setSearchResult] = useState(null);
-  const [showSearch, setShowSearch] = useState(false);
+  const [searchResult, setSearchResult] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [userId, setUserId] = useState(null); // State to store authenticated user's ID
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,24 +26,28 @@ function Dashboard() {
       }
 
       const userId = user.uid;
-      console.log("Authenticated User ID:", userId); // Log user ID
+      setUserId(userId); // Store user ID in state
+      console.log("Authenticated User ID:", userId);
 
       try {
-        const querySnapshot = await getDocs(collection(db, `users/${userId}/selectedOptions`));
+        const querySnapshot = await getDocs(
+          collection(db, `users/${userId}/selectedOptions`),
+        );
+        console.log("Query Snapshot:", querySnapshot);
 
         if (querySnapshot.empty) {
           console.log("No documents found in the collection.");
-          setData([]); // Ensure data is set to an empty array if no documents are found
+          setData([]);
         } else {
           console.log("Documents found in the collection.");
           const dataList = querySnapshot.docs.map((doc) => {
-            console.log("Document Data:", doc.data()); // Log document data
+            console.log("Document Data:", doc.data());
             return {
               id: doc.id,
               ...doc.data(),
             };
           });
-          console.log("Fetched Data List:", dataList); // Log fetched data list
+          console.log("Fetched Data List:", dataList);
           setData(dataList);
         }
       } catch (error) {
@@ -65,129 +70,140 @@ function Dashboard() {
     }
   };
 
-  const handleSearch = () => {
-    if (!searchId) {
-      setSearchError("Please enter a User ID");
+  const handleSearch = async () => {
+    if (!inputValue) {
+      setSearchError("Please enter a User ID(s)");
       return;
     }
 
-    console.log("Searching for ID:", searchId);
-    console.log("Current Data:", data);
+    const ids = inputValue.split(",").map((id) => id.trim());
+    setSearchIds(ids);
+    console.log("Searching for IDs:", ids);
 
-    const result = data.find((item) => item.id === searchId);
-    console.log("Search Result:", result);
+    const newResults = [];
 
-    setSearchResult(result || "No data found");
-    setSearchError(null); // Clear previous error
+    for (const id of ids) {
+      try {
+        const querySnapshot = await getDocs(
+          collection(db, `users/${id}/selectedOptions`),
+        );
+
+        if (querySnapshot.empty) {
+          console.log(`No documents found for User ID: ${id}`);
+          newResults.push({ id, data: [] });
+        } else {
+          const dataList = querySnapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }));
+          newResults.push({ id, data: dataList });
+        }
+      } catch (error) {
+        console.error(`Error fetching data for User ID ${id}: `, error);
+        newResults.push({ id, error: error.message });
+      }
+    }
+
+    setSearchResult(newResults.concat(searchResult));
   };
 
-  const handleClearSearch = () => {
-    setSearchId("");
-    setSearchResult(null);
-    setSearchError(null); // Clear previous error
+  const clearSearchResults = () => {
+    setSearchIds([]);
+    setSearchResult([]);
   };
-
-  if (loading) {
-    return <h1>Loading...</h1>;
-  }
-
-  if (error) {
-    return <h1>{error}</h1>;
-  }
 
   return (
     <div className={styles.dashboard}>
       <div className={styles.header}>
-        <h2 className={styles.welcome}>
-          Welcome, {getAuth().currentUser?.email || "Employer"}
-        </h2>
-        <button onClick={handleLogout} className={styles.logoutButton}>
+        <div className={styles.welcome}>
+          Welcome, {getAuth().currentUser?.email || "User"}
+        </div>
+        <button className={styles.logoutButton} onClick={handleLogout}>
           Logout
         </button>
       </div>
-      <h1>Your Dashboard</h1>
-      <button
-        onClick={() => setShowSearch(!showSearch)}
-        className={styles.toggleSearchButton}
-      >
-        {showSearch ? "Hide Search" : "Show Search"}
-      </button>
-      {showSearch && (
-        <div className={styles.searchSection}>
+
+      <h1>Dashboard</h1>
+      <div className={styles.searchSection}>
+        <div className={styles.inputContainer}>
           <input
             type="text"
-            className={styles.searchInput}
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
             placeholder="Enter User ID"
-            value={searchId}
-            onChange={(e) => setSearchId(e.target.value)}
+            className={styles.searchInput}
           />
-          <button onClick={handleSearch} className={styles.searchButton}>
-            Search
-          </button>
-          <button onClick={handleClearSearch} className={styles.clearButton}>
-            Clear Search
-          </button>
-          {searchError && <p className={styles.error}>{searchError}</p>}
         </div>
-      )}
-      {searchResult && (
-        <div className={styles.searchResult}>
-          <h3>Search Result</h3>
-          {typeof searchResult === "string" ? (
-            <p>{searchResult}</p>
-          ) : (
-            <table className={styles.table}>
-              <thead>
-                <tr>
-                  <th>Question</th>
-                  <th>Source</th>
-                  <th>Option</th>
-                  <th>Timestamp</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>{searchResult.question}</td>
-                  <td>{searchResult.source}</td>
-                  <td>{searchResult.option}</td>
-                  <td>
-                    {new Date(
-                      searchResult.timestamp.seconds * 1000,
-                    ).toLocaleString()}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          )}
-        </div>
-      )}
-      <div className={styles.dataSection}>
-        <h3>All Data</h3>
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Question</th>
-              <th>Source</th>
-              <th>Option</th>
-              <th>Timestamp</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item) => (
-              <tr key={item.id}>
-                <td>{item.id}</td>
-                <td>{item.question}</td>
-                <td>{item.source}</td>
-                <td>{item.option}</td>
-                <td>
-                  {new Date(item.timestamp.seconds * 1000).toLocaleString()}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <h6 className={styles.multipleId}>
+          search multiple IDs by separating with a comma
+        </h6>
+        <button className={styles.searchButton} onClick={handleSearch}>
+          Search
+        </button>
+
+        <button className={styles.clearButton} onClick={clearSearchResults}>
+          Clear
+        </button>
+        {searchError && <div className={styles.error}>{searchError}</div>}
       </div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div className={styles.error}>{error}</div>
+      ) : (
+        <div></div>
+      )}
+      {searchResult.length > 0 && (
+        <div className={styles.searchResult}>
+          {searchResult.map((result) => (
+            <div key={result.id}>
+              <div className={styles.resultHeader}>
+                <h3>User ID: {result.id}</h3>
+                <button
+                  className={styles.deleteButton}
+                  onClick={() =>
+                    setSearchResult(
+                      searchResult.filter((r) => r.id !== result.id),
+                    )
+                  }
+                >
+                  ×
+                </button>
+              </div>
+              {result.error ? (
+                <div className={styles.error}>{result.error}</div>
+              ) : (
+                <table className={styles.table}>
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Option</th>
+                      <th>Source</th>
+                      <th>Option</th>
+                      <th>Timestamp</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {result.data.map((item) => (
+                      <tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>{item.option}</td>
+                        <td>{item.source}</td>
+                        <td>{item.option}</td>
+                        <td>
+                          {new Date(
+                            item.timestamp.seconds * 1000,
+                          ).toLocaleString()}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              )}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
