8eaa841f9b1f1eff56680c19b2dab5a7e19d2af8
akosiaxcel <117887168+akosiaxcel@users.noreply.github.com>
2024-07-30 19:12:01 +0000

    nav adjust

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { getAuth, signOut } from "firebase/auth";
 import styles from "./NavBar.module.css";
 
-
 function NavBar({ onLogout }) {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
   const handleLogout = async () => {
     const auth = getAuth();
     try {
@@ -14,28 +15,45 @@ function NavBar({ onLogout }) {
     }
   };
 
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className={styles.navbar}>
-      <div className={styles.left}>
-        <img
-          src="https://www.scaleupconsulting.com.au/wp-content/uploads/2022/05/ScaleupConsulting_Logo_Primary-3.png"
-          alt="Company Logo"
-          className={styles.logo}
-        />
+    <>
+      <div className={styles.navbar}>
+        <div className={styles.right}>
+          <img
+            src="https://www.scaleupconsulting.com.au/wp-content/uploads/2022/05/ScaleupConsulting_Logo_Primary-3.png"
+            alt="Company Logo"
+            className={styles.logo}
+          />
+        </div>
+        <div className={styles.menuIcon} onClick={toggleSidebar}>
+          &#9776;
+        </div>
+        <div className={styles.logoutButtonContainer}>
+          <button className={styles.logoutButton} onClick={handleLogout}>
+            Logout
+          </button>
+        </div>
       </div>
-      {/* <div className={styles.profile}>
-        <img
-          src={logoUrl}
-          alt="Profile"
-          className={styles.profilePic}
-        />
-      </div> */}
-      <div className={styles.logoutButtonContainer}>
+      <div
+        className={`${styles.sidebar} ${isSidebarOpen ? styles.open : ""}`}
+        id="sidebar"
+      >
+        <a href="#" className={styles.closebtn} onClick={toggleSidebar}>
+          &times;
+        </a>
+        {/* <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Contact</a> */}
         <button className={styles.logoutButton} onClick={handleLogout}>
           Logout
         </button>
       </div>
-    </div>
+    </>
   );
 }
 
